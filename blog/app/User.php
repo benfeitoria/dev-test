115536c7a442701e1d48705f425c9caa81c8bf4d
renan@renanpigato.inf.br <renan@renanpigato.inf.br>
2020-05-18 11:26:09 -0300

    Fonte movido

--- blog/app/User.php
+++ blog/app/User.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Model;
+namespace App;
 
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
--- blog/database/factories/UserFactory.php
+++ blog/database/factories/UserFactory.php
@@ -2,7 +2,7 @@
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
-use App\Model\User;
+use App\User;
 use Faker\Generator as Faker;
 use Illuminate\Support\Str;
 
