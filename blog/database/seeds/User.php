6b748f1db42834d680bec87fd6c7427f4edf02d2
renan@renanpigato.inf.br <renan@renanpigato.inf.br>
2020-05-17 21:07:34 -0300

    Dados para tabela de usuário e de postagem

---
+++ blog/database/seeds/User.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class User extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(App\Model\User::class, 2)->create();
+    }
+}
