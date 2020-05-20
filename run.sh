e06f3af47bd3bf69e29c509e41ab821db74edbd3
renan@renanpigato.inf.br <renan@renanpigato.inf.br>
2020-05-20 15:09:05 -0300

    Correção no shell script

--- run.sh
+++ run.sh
@@ -11,6 +11,13 @@ then
   ln -s blog/public/ public
 fi
 
+if [[ ! -d "blog/storage/framework" ]]
+then
+  mkdir -p blog/storage/framework/cache/data
+  mkdir -p blog/storage/framework/sessions
+  mkdir -p blog/storage/framework/views
+fi
+
 if [[ ! -f "blog/.env" ]]
 then
   cp blog/.env.example blog/.env
