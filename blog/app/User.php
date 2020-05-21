df2324e6bb70d77d4d836b6d30b86b90e1ea0144
renan@renanpigato.inf.br <renan@renanpigato.inf.br>
2020-05-21 18:45:34 -0300

    Retornada versão e retirado pacote passport

--- blog/app/User.php
+++ blog/app/User.php
@@ -5,11 +5,10 @@ namespace App;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Laravel\Passport\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, Notifiable;
+    use Notifiable;
 
     /**
      * The attributes that are mass assignable.
